a1b0f79400ef5cc5948ffa648902876d697fb192
louxiaojian <louxiaojian@a9a8adc8-fc0a-11de-9905-3f11d370f582>
2012-12-16 12:45:12 +0000

    @增加数据库自动备份bat

---
+++ trunk/php/notes/backup_db.bat
@@ -0,0 +1,24 @@
+@echo off
+echo *****************************
+echo.
+echo 今天是 %date%
+echo 时间是 %time%
+echo.
+echo.
+echo *****************************
+
+echo %date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%
+
+set filename=notes%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%.sql
+
+echo %filename%
+
+d:
+
+cd Program Files\MySQL\MySQL Server 5.5\bin
+
+mysqldump.exe notes -uroot -p123456 > E:\back-end\PHP\PHPRoot\MyLab\notes\SQL\%filename%
+
+echo notes数据库备份至DE:\back-end\PHP\PHPRoot\MyLab\notes\SQL\%filename%
+
+pause & exit 
